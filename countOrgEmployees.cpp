afc89672fe4cc4de1a6443a8911e1c3e0f1c41d7
crkntk <crkntk@gmail.com>
2024-02-03 20:24:56 -0800

    code explanation

--- countOrgEmployees.cpp
+++ countOrgEmployees.cpp
@@ -16,6 +16,7 @@ int parse_max(const char *maxLevelPath){
             maxLevel = *initPointer - 48;
         }
     }
+    //deallocates memory of initPointer
     return maxLevel;
     initPointer = nullptr;
     delete(initPointer);
@@ -31,36 +32,37 @@ int main(int argc, char **argv){
     lineNum = 0;
     orgchart *root;
     while (getline(firstFile, line)){ // Constructs the tree by reading in each line in first argument file
-        if(lineNum == 0){
+        if(lineNum == 0){  //This is to get the initial line with maximum levels allowed
             maxL = parse_max(line.c_str());
             lineNum++;
             continue;
         }
-        if(lineNum == 1){
+        if(lineNum == 1){ // this is to pass the second line and create root node; second line has maxes per level of tree
             root = new orgchart(line.c_str(), maxL);
             lineNum++;
             continue;
         }
-        root->addEmployees(line.c_str());
+        root->addEmployees(line.c_str()); // adds employees of the first argument to create the tree from file path
         lineNum++;
     }
-    firstFile.close();
+    firstFile.close(); //closes first file
     ifstream secondFile(testFilePath);
     orgchart *currNode;
     while (getline(secondFile, line)){ //processes the second argument file and finds node and counts its employees
         unsigned count = 1;
-        currNode = root->findEmployee(line.c_str());
-        if(currNode == nullptr || currNode == NULL){
+        currNode = root->findEmployee(line.c_str()); //finds employee from the first line of the argument
+        if(currNode == nullptr || currNode == NULL){ //when the find employee function returns null pointer check mean no employee found
             cout << line.c_str();
         }
         else {
-            cout << currNode->employeeID;
+            cout << currNode->employeeID; //access found employee that is not null pointer
         }
+        //processes when the function doesnt find employee so count is zero
         if(currNode == nullptr){
             count = 0;
         }
         else {
-            currNode->countNumOfEmployeesInOrgchart(count);
+            currNode->countNumOfEmployeesInOrgchart(count); // counts the employee by using pass by reference
         }
         cout << " " ;
         cout << count;
--- orgchart.cpp
+++ orgchart.cpp
@@ -8,16 +8,16 @@
 
 orgchart:: orgchart(const char *maxChildrenPath, unsigned int levelMax){
     //Constructor for Root node
-    maxChild = 0;
+    maxChild = 0; //initialization for maxChild root node doesnt need this so its set to zero
     const char *initPointer;
-    maxLevel = levelMax;
-    maxChildren = new int[levelMax];
+    maxLevel = levelMax; //This is the max levels allowed on the tree
+    maxChildren = new int[levelMax]; //initialization for array of the node level of allowed max children for each node
     int counter = 0;
     children = nullptr;
-    char* resultID = new char[3];
+    char* resultID = new char[3]; //Id of the root node initialization and copying
     strcpy(resultID, "e_\0");
     employeeID = resultID;
-    for(initPointer = maxChildrenPath; *initPointer != '\0'; initPointer++){
+    for(initPointer = maxChildrenPath; *initPointer != '\0'; initPointer++){ // parsing to creating the maxChildren per node array
         if(isdigit(*initPointer) && *initPointer != ' '){
             maxChildren[counter] = *initPointer - 48;
             counter++;
@@ -26,7 +26,7 @@ orgchart:: orgchart(const char *maxChildrenPath, unsigned int levelMax){
     initPointer = nullptr;
     delete(initPointer);
     children =  new orgchart*[maxChildren[0]];
-    for(int i = 0; i < maxChildren[0]; i++){
+    for(int i = 0; i < maxChildren[0]; i++){ //initializing the children
         children[i] = nullptr;
     }
     resultID = nullptr;
@@ -36,13 +36,13 @@ orgchart::orgchart(unsigned int maxCh, const char *Id, int lenID){
     //Constructor for child nodes
     children = nullptr;
     maxLevel = 0;
-    maxChild = maxCh;
+    maxChild = maxCh; //Set max children for current node
     employeeID = Id;
-    char* resultID = new char[lenID + 1];
-    strcpy(resultID, Id);
-    employeeID = resultID;
+    char* resultID = new char[lenID + 1]; //instantiating the id
+    strcpy(resultID, Id); // copying id
+    employeeID = resultID; //seting id
     children =  new orgchart*[maxChild];
-    for(unsigned int i = 0; i< maxCh; i++){
+    for(unsigned int i = 0; i< maxCh; i++){ // instantiating children array
         children[i] = nullptr;
     }
     resultID = nullptr;
@@ -52,12 +52,13 @@ orgchart::orgchart(unsigned int maxCh, const char *Id, int lenID){
 bool orgchart::addEmployees(const char *employeeIDPath){
     //This function adds employees starting from root down the path stops when invalid and keeps the past added notes
     const char *initPointer;
-    int currLevel = 1;
-    orgchart* currNode = this;
-    int IDLen = 2;
-    char* resultID = new char[IDLen + 1];
-    strcpy(resultID, "e_");
+    int currLevel = 1; // root node is the zero level so we start at level 1 of the tree
+    orgchart* currNode = this; // sets the curr node to parse from current node
+    int IDLen = 2; // the length of the id
+    char* resultID = new char[IDLen + 1]; // instantiating with the ending character \0
+    strcpy(resultID, "e_"); //since root node already made we start with this parse string
     for(initPointer = employeeIDPath+2; *initPointer != '\0'; initPointer++){
+        //starts at first id since we already have the root node
         char* teRes = add_char(resultID, *initPointer, IDLen);
       //This frees up memory in case we have our pointer not the same as our result id we want for the node
         if (teRes != resultID) {
@@ -65,22 +66,23 @@ bool orgchart::addEmployees(const char *employeeIDPath){
         }
         resultID = teRes;
         IDLen++;
-        if(!isdigit(*initPointer) && *initPointer != '_'){
+        if(!isdigit(*initPointer) && *initPointer != '_'){ // checks if invalid character so this for loop is stopped
             break;
         }
-        if(isdigit(*initPointer) && (maxChildren[currLevel - 1] <= ((int)*initPointer - 48))){
+        if(isdigit(*initPointer) && (maxChildren[currLevel - 1] <= ((int)*initPointer - 48))){ // checks for if its a digit and its max children for node
             break;
         }
         if(maxLevel < currLevel || (((*initPointer != '_' && !isdigit(*initPointer))) && (maxChildren[currLevel - 1] < ((int)*initPointer - 48)))){
+            // checks the max level and curent level and if its also not a valid id
         break;
         }
         if(isdigit(*initPointer)) {
-            if ((currNode->children[(int) *initPointer - 48] != nullptr)) {
+            if ((currNode->children[(int) *initPointer - 48] != nullptr)) { //if the node exists
                 currNode = currNode->children[(int) *initPointer - 48];
                 currLevel++;
                 continue;
             }
-            if (currNode->children[(int) *initPointer - 48] == nullptr) {
+            if (currNode->children[(int) *initPointer - 48] == nullptr) { //it node doesnt exist it creates it
                 currNode->children[(int) *initPointer - 48] = new orgchart(maxChildren[currLevel], resultID, IDLen);
                 currNode = currNode->children[(int) *initPointer - 48];
                 currLevel++;
@@ -100,26 +102,26 @@ orgchart* orgchart::findEmployee(const char *employeeIDPath){
     int currLevel = 1;
     for(initPointer = employeeIDPath + 2; *initPointer != '\0'; initPointer++){
         // this loop iterates over the path and starts at first level
-        if(!isdigit(*initPointer) && *initPointer != '_'){
+        if(!isdigit(*initPointer) && *initPointer != '_'){ //checks if its not a valid id in the string
             return nullptr;
         }
-        if(currNode == NULL || currNode == nullptr){
-            return nullptr;
-            break;
-        }
-        if((*initPointer != '_' && !isdigit(*initPointer))){
-            break;
-        }
-        if(isdigit(*initPointer) && (maxChildren[currLevel-1] <= ((int)*initPointer - 48))){
+        if(currNode == NULL || currNode == nullptr){//if the node down the path doesnt exist it exits and return nullptr
             return nullptr;
             break;
         }
-        else if(isdigit(*initPointer) && (*initPointer != '_')) {
-            if ((currNode->children[(int) *initPointer - 48] != nullptr)) {
+        if((*initPointer != '_' && !isdigit(*initPointer))){ //checks again if it has a valid id
+            break;
+        }
+        if(isdigit(*initPointer) && (maxChildren[currLevel-1] <= ((int)*initPointer - 48))){ //checks max children
+            return nullptr;
+            break;
+        }
+        else if(isdigit(*initPointer) && (*initPointer != '_')) { //valid id
+            if ((currNode->children[(int) *initPointer - 48] != nullptr)) {//if we have a valid node found we go down the tree
                 currNode = currNode -> children[(int) *initPointer - 48];
                 currLevel++;
             }
-            else if (currNode->children[(int) *initPointer - 48] == nullptr) {
+            else if (currNode->children[(int) *initPointer - 48] == nullptr) { //if the node doesnt exist we reutrn null ptr
                 return nullptr;
             }
         }
@@ -131,13 +133,13 @@ orgchart* orgchart::findEmployee(const char *employeeIDPath){
 void orgchart::countNumOfEmployeesInOrgchart (unsigned int &count){
     // This function counts the employees under the current node and visits and counts in bfs order
 
-    orgchart* currNode = this;
-    if(currNode == nullptr || currNode->children == NULL){
+    orgchart* currNode = this;//starts at the curren node to iterate
+    if(currNode == nullptr || currNode->children == NULL){ //if we have a null node then we exit
         return;
     }
     orgchart* curNode;
 
-    for(int curr = 0; curr != maxChild; curr++){ // Visits and counts each child
+    for(int curr = 0; curr != maxChild; curr++){ // Visits and counts each child that is not a null ptr
         if(currNode->children[curr] == nullptr){
             continue;
         }
@@ -146,7 +148,7 @@ void orgchart::countNumOfEmployeesInOrgchart (unsigned int &count){
         }
     }
 
-    for(int curr = 0; curr != maxChild; curr++){ // Visits the children
+    for(int curr = 0; curr != maxChild; curr++){ // Visits the children recursively
         curNode = children[curr];
         if(currNode->children[curr] == nullptr){
             continue;
@@ -155,6 +157,7 @@ void orgchart::countNumOfEmployeesInOrgchart (unsigned int &count){
     }
 }
 char* orgchart::add_char(char *first, char second, int lenFirst ){
+    //This functions copies onto another string
     char *result = new char[lenFirst+ 2];
     char endStr[2];
     endStr[0]=second;
